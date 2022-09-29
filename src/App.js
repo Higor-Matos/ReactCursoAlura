46b9a3a43ed920467a83781b1386191243714d6a
Higor de Deus <higordeus22@gmail.com>
2022-09-29 15:25:27 -0300

    "Lista agora tem layout"

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario';
 import { useState } from 'react'
+import Time from './componentes/Time';
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Games',
+      corPrimaria: '#50fa7b',
+      corSecundaria: '#44475a'
+    },
+    {
+      nome: 'Animes',
+      corPrimaria: '#ffb86c',
+      corSecundaria: '#6272a4'
+    },
+    {
+      nome: 'Desenhos',
+      corPrimaria: '#50fa7b',
+      corSecundaria: '#44475a'
+    },
+    {
+      nome: 'Séries',
+      corPrimaria: '#ffb86c',
+      corSecundaria: '#6272a4'
+    },
+    {
+      nome: 'Filmes',
+      corPrimaria: '#50fa7b',
+      corSecundaria: '#44475a'
+    }
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -15,6 +44,7 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario aoColaboradorCadastrado = {colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
     </div>
   );
 }
