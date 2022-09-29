93f06ff8b4ec8243d0f7e0d6e2a6fb6b67b76427
Higor de Deus <higordeus22@gmail.com>
2022-09-29 10:54:12 -0300

    "Formulario já disponibiliza informações digitadas"

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,6 +2,7 @@ import CampoTexto from '../Banner/CampoTexto'
 import Botao from '../Botao'
 import ListaSuspensa from '../ListaSuspensa'
 import './Formulario.css'
+import { useState } from 'react'
 
 const Formulario = () => {
 
@@ -12,14 +13,42 @@ const Formulario = () => {
         'Séries',
         'Filmes'
     ]
+
+    const [nome, setNome] = useState('')
+    const [origem, setOrigem] = useState('')
+    const [imagem, setImagem] = useState('')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log('Form foi submetido =>', nome, origem, imagem);
+    }
+
     return(
         <section className = "formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2> Preencha os dados para criar o card do personagem </h2>
-            <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-            <CampoTexto label="Origem" placeholder="Digite sua origem"/>
-            <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-            <ListaSuspensa label="Time" itens = {times}/>
+            <CampoTexto  
+                obrigatorio={true}
+                label="Nome" 
+                placeholder="Digite seu nome"
+                valor={nome}
+                aoAlterado={valor => setNome(valor)}
+            />
+            <CampoTexto 
+                obrigatorio={true} 
+                label="Origem" 
+                placeholder="Digite sua origem"
+                valor={origem}
+                aoAlterado={valor => setOrigem(valor)}
+            />
+            <CampoTexto 
+                obrigatorio={true}
+                label="Imagem" 
+                placeholder="Digite o endereço da imagem"
+                valor={imagem}
+                aoAlterado={valor => setImagem(valor)}
+            />
+            <ListaSuspensa obrigatorio={true}label="Time" itens = {times}/>
             <Botao texto="Criar Card"></Botao>
             </form>
         </section>
